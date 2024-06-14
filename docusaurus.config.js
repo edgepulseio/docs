9fd5bfd4ba5e1d1de508abfa590012910905d8ca
Cyril Scetbon <cscetbon@gmail.com>
2024-06-14 19:55:21 -0400

    Fix tagline

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ import { themes as prismThemes } from 'prism-react-renderer'
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'EdgePulse',
-  tagline: 'Dinosaurs are cool',
+  tagline: 'EdgePulse framework documentation',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
