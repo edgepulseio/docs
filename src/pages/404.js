51a96ba0d272ea8bf6d030cc904da8ecb413fa0a
Cyril Scetbon <cscetbon@gmail.com>
2024-06-14 17:29:42 -0400

    Redirect to /docs not /

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,5 +2,5 @@ import { Redirect } from '@docusaurus/router'
 import React from 'react'
 
 export default function NotFound() {
-    return <Redirect to="/" />
+    return <Redirect to="/docs" />
 }
